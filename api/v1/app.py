20a6d364316e28ad03bcc8e48f1bbd262dc9b245
Adrian <dreamweaver3d@gmail.com>
2023-12-01 09:55:49 +0800

    airbnb API task 4 fixed pycodestyle complaints

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,6 +11,7 @@ app = Flask(__name__)
 
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown_db(exception):
     """closes the storage on teardown"""
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,7 +4,8 @@ from flask import jsonify
 
 from api.v1.views.__init__ import app_views
 
+
 @app_views.route('/status', strict_slashes=False)
 def status():
     """returns status ok"""
-    return jsonify({"status":"OK"})
+    return jsonify({"status": "OK"})
