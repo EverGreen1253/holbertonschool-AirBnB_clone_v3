333eebe3dc7c5f3e9381135880435cae01ac699a
Adrian <dreamweaver3d@gmail.com>
2023-12-01 09:53:18 +0800

    airbnb API task 4 got the routes working by importing app_views correctly

---
+++ api/v1/app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+starts a Flask web application
+"""
+
+from models import storage
+from os import getenv
+from api.v1.views.__init__ import app_views
+from flask import Flask, Blueprint
+app = Flask(__name__)
+
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """closes the storage on teardown"""
+    storage.close()
+
+if __name__ == "__main__":
+    host = getenv('HBNB_API_HOST')
+    port = getenv('HBNB_API_PORT')
+
+    if host is None:
+        host = '0.0.0.0'
+
+    if port is None:
+        port = '5000'
+
+    app.run(host=host, port=port, threaded=True)
---
+++ api/v1/views/index.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+from flask import jsonify
+
+from api.v1.views.__init__ import app_views
+
+@app_views.route('/status', strict_slashes=False)
+def status():
+    """returns status ok"""
+    return jsonify({"status":"OK"})
