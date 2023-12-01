2e2b2303b0ab283aac322688b1e107f6fed8c3df
Adrian <dreamweaver3d@gmail.com>
2023-12-01 12:22:26 +0800

    airbnb API task 5 fixed stats API endpoint output

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Index file"""
 from flask import jsonify
-import models
+from models.__init__ import storage
 from models import amenity as Amenity
 from models import city as City
 from models import place as Place
@@ -17,16 +17,17 @@ def status():
     """returns status ok"""
     return jsonify({"status": "OK"})
 
+
 @app_views.route('/stats', strict_slashes=False)
 def stats():
     """returns statistics"""
 
     data = {
-        "amenities": len(models.storage.all(Amenity)),
-        "cities": len(models.storage.all(City)),
-        "places": len(models.storage.all(Place)),
-        "reviews": len(models.storage.all(Review)),
-        "states": len(models.storage.all(State)),
-        "users": len(models.storage.all(User))
+        "amenities": len(storage.all('Amenity')),
+        "cities": len(storage.all('City')),
+        "places": len(storage.all('Place')),
+        "reviews": len(storage.all('Review')),
+        "states": len(storage.all('State')),
+        "users": len(storage.all('User'))
     }
     return jsonify(data)
