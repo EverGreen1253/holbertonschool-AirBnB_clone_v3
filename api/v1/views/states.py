fd3c020ad5a29affe3783fd92c9fa4576cc8077c
Adrian <dreamweaver3d@gmail.com>
2023-12-04 12:22:39 +0800

    airbnb restful API task 7 added DELETE req support

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """States file"""
-from flask import jsonify
+from flask import jsonify, abort
 from models import storage
 from api.v1.views.__init__ import app_views
 
 class_name = "State"
+
 
 @app_views.route('/states', strict_slashes=False)
 def states():
@@ -26,12 +27,15 @@ def states():
     return jsonify(data)
 
 
-@app_views.route('/states/<state_id>', strict_slashes=False)
-def state_specific(state_id):
+@app_views.route('/states/<state_id>', strict_slashes=False, methods=["GET"])
+def get_specific_state(state_id):
     """returns specified State"""
     data = {}
 
     v = storage.get(class_name, state_id)
+    if v is None:
+        abort(404)
+
     data = {
         "__class__": class_name,
         "created_at": v.created_at,
@@ -41,3 +45,35 @@ def state_specific(state_id):
     }
 
     return data
+
+
+@app_views.route('/states/<state_id>', strict_slashes=False, methods=["DELETE"])
+def del_specific_state(state_id):
+    """returns specified State"""
+    v = storage.get(class_name, state_id)
+    if v is None:
+        abort(404)
+
+    storage.delete(v)
+
+    return {}, 200
+
+
+# @app_views.route('/states/<state_id>', strict_slashes=False, methods=["POST"])
+# def post_specific_state(state_id):
+#     """returns specified State"""
+#     data = {}
+
+#     v = storage.get(class_name, state_id)
+#     if v is None:
+#         abort(404)
+
+#     data = {
+#         "__class__": class_name,
+#         "created_at": v.created_at,
+#         "id": v.id,
+#         "name": v.name,
+#         "updated_at": v.updated_at
+#     }
+
+#     return data
