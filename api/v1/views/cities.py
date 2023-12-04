ab865ce4b30f4487661a55fca2d7c8575405f5e7
Adrian <dreamweaver3d@gmail.com>
2023-12-04 14:43:46 +0800

    airbnb restful API task 8 City POST route fixed 3

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -74,6 +74,9 @@ def post_specific_city(state_id):
     """saves specified City"""
     from models.state import City
 
+    if state_id is None:
+        abort(404)
+
     existing = storage.get("State", state_id)
     if existing is None:
         abort(404)
